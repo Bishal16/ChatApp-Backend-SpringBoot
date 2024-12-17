cb3d957df1638baff285ed7a42f9ae9abe60e154
bishal16 <mahathirbishal@gmail.com>
2024-12-17 17:12:13 +0600

    trying 03

--- src/test/java/dev/mahathir/chatappbackend/ChatappSpringbootApplicationTests.java
+++ src/test/java/dev/mahathir/chatappbackend/ChatappSpringbootApplicationTests.java
@@ -3,11 +3,11 @@ package dev.mahathir.chatappbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChatappSpringbootApplication.class)
 class ChatappSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
